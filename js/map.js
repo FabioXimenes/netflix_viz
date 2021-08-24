13b46d6ccf06b685eef4d7797112e57557a78389
jvictorsouza <victor.souza@lapisco.ifce.edu.br>
2021-08-24 15:07:01 -0300

    adjust min zoom limit

--- js/map.js
+++ js/map.js
@@ -21,13 +21,13 @@ export function map(facts, dataset) {
     });
 
     let map = L.map("mapid").setView([21.511116, -10.671271], 2);
+    map.options.minZoom = 2;
+    map.options.maxZoom = 6;
     L.tileLayer(
       "https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoianZpY3RvcnNvdXphIiwiYSI6ImNrZ2dzZnpwZTA0MnMycXF5dHk0ZTdweG8ifQ.ZIydKbNs6Ji-_Cligx2uAA",
       {
         attribution:
           'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 5,
-        minZomm: 2,
       }
     ).addTo(map);
 
