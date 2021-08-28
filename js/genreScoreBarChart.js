0c1320bddeb4e990d8f248c1ccee27984ff6b81c
jvictorsouza <victor.souza@lapisco.ifce.edu.br>
2021-08-28 20:17:12 -0300

    add axis labels

--- js/genreScoreBarChart.js
+++ js/genreScoreBarChart.js
@@ -43,11 +43,49 @@ export function genreScoreBarChart(dataset) {
 
   let topKScoreGroup = getTops(scoreGroup, 10);
 
+  // Functions to add x-label & y-label to Row Charts (Unsupported by dc.js)
+	var addXLabel = function (chartToUpdate, displayText) {
+		var textSelection = chartToUpdate.svg()
+			.append("text")
+			.attr("class", "x-axis-label-rowBarChart")
+			.attr("text-anchor", "middle")
+			.attr("x", chartToUpdate.width() / 2)
+			.attr("y", chartToUpdate.height() - 10)
+			.text(displayText);
+		var textDims = textSelection.node().getBBox();
+		var chartMargins = chartToUpdate.margins();
+
+		// Dynamically adjust positioning after reading text dimension from DOM
+		textSelection
+			.attr("x", chartMargins.left + (chartToUpdate.width()
+				- chartMargins.left - chartMargins.right) / 2)
+			.attr("y", chartToUpdate.height() - Math.ceil(textDims.height) / 2);
+	};
+	var addYLabel = function (chartToUpdate, displayText) {
+		var textSelection = chartToUpdate.svg()
+			.append("text")
+			.attr("class", "y-axis-label-rowBarChart")
+			.attr("text-anchor", "middle")
+			.attr("transform", "rotate(-90), translate(0, -85)")
+			.attr("x", -chartToUpdate.height() / 2)
+			.attr("y", 10)
+			.text(displayText);
+		var textDims = textSelection.node().getBBox();
+		var chartMargins = chartToUpdate.margins();
+
+		// Dynamically adjust positioning after reading text dimension from DOM
+		textSelection
+			.attr("x", -chartMargins.top - (chartToUpdate.height()
+				- chartMargins.top - chartMargins.bottom) / 2)
+			.attr("y", Math.max(Math.ceil(textDims.height), chartMargins.left
+				- Math.ceil(textDims.height) - 5));
+	};
+
   var barChart = dc.rowChart("#genre-score-bar-chart");
   barChart
     .height(300)
     .width(800)
-    .margins({ top: 10, right: 70, bottom: 20, left: 100 })
+    .margins({ top: 10, right: 70, bottom: 50, left: 130 })
     .labelOffsetX(-5)
     .on("renderlet", function (chart) {
       chart
@@ -77,6 +115,11 @@ export function genreScoreBarChart(dataset) {
 
     barChart.xAxis().tickFormat(function (v) {return v + '%';});
     barChart.xAxis().tickValues([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]);
+
+    barChart.on("postRender", function (chart) {
+      addXLabel(chart, "Tomatometer Score");
+      addYLabel(chart, "Genres");
+    });
 }
 
 function splitGenres(dataset) {
--- js/ratingTypeHorizontalBarChart.js
+++ js/ratingTypeHorizontalBarChart.js
@@ -5,16 +5,54 @@ export function ratingTypeHorizontalBarChart(facts) {
 	let ratingTypeDimensionGroup = ratingTypeDimension.group()
 	let ratingTypesNames = ratingTypeDimensionGroup.top(Infinity).map(d => d.key)
 
+	// Functions to add x-label & y-label to Row Charts (Unsupported by dc.js)
+	var addXLabel = function (chartToUpdate, displayText) {
+		var textSelection = chartToUpdate.svg()
+			.append("text")
+			.attr("class", "x-axis-label-rowBarChart")
+			.attr("text-anchor", "middle")
+			.attr("x", chartToUpdate.width() / 2)
+			.attr("y", chartToUpdate.height() - 10)
+			.text(displayText);
+		var textDims = textSelection.node().getBBox();
+		var chartMargins = chartToUpdate.margins();
+
+		// Dynamically adjust positioning after reading text dimension from DOM
+		textSelection
+			.attr("x", chartMargins.left + (chartToUpdate.width()
+				- chartMargins.left - chartMargins.right) / 2)
+			.attr("y", chartToUpdate.height() - Math.ceil(textDims.height) / 2);
+	};
+	var addYLabel = function (chartToUpdate, displayText) {
+		var textSelection = chartToUpdate.svg()
+			.append("text")
+			.attr("class", "y-axis-label-rowBarChart")
+			.attr("text-anchor", "middle")
+			.attr("transform", "rotate(-90), translate(0, -30)")
+			.attr("x", -chartToUpdate.height() / 2)
+			.attr("y", 10)
+			.text(displayText);
+		var textDims = textSelection.node().getBBox();
+		var chartMargins = chartToUpdate.margins();
+
+		// Dynamically adjust positioning after reading text dimension from DOM
+		textSelection
+			.attr("x", -chartMargins.top - (chartToUpdate.height()
+				- chartMargins.top - chartMargins.bottom) / 2)
+			.attr("y", Math.max(Math.ceil(textDims.height), chartMargins.left
+				- Math.ceil(textDims.height) - 5));
+	};
+
 	let ratingTypeBarChart = dc.rowChart("#rating-type-chart")
 	ratingTypeBarChart.width(650)
 		.height(350)
-		.margins({top: 10, right: 50, bottom: 40, left: 80})
+		.margins({ top: 10, right: 50, bottom: 50, left: 80 })
 		.labelOffsetX(-5)
 		.x(d3.scaleLinear()
-					.domain([0, ratingTypeDimensionGroup.top(1)[0].value])
-					.range([0, ratingTypeBarChart.effectiveWidth()])
-					.clamp(true)
-			)
+			.domain([0, ratingTypeDimensionGroup.top(1)[0].value])
+			.range([0, ratingTypeBarChart.effectiveWidth()])
+			.clamp(true)
+		)
 		.on('renderlet', function (chart) {
 			chart.selectAll("g.row  text")
 				.style("text-anchor", "end")
@@ -35,35 +73,14 @@ export function ratingTypeHorizontalBarChart(facts) {
 		.gap(2)
 		.title('Total')
 		.ordinalColors(['#d62728'])
-		.on("filtered", function(chart,filter){
+		.on("filtered", function (chart, filter) {
 			updateMarkers()
 		});
-	
-	// addXAxis(ratingTypeBarChart, 'Total')
-	// addYAxis(ratingTypeBarChart, 'Rating')
-	
 
-}
+	ratingTypeBarChart.on("postRender", function (chart) {
+		addXLabel(chart, "Number of Productions");
+		addYLabel(chart, "Rating");
+	});
 
-function addXAxis(chartToUpdate, displayText) {
-	console.log(chartToUpdate)
-	chartToUpdate
-		// .selectAll("g.row  text")
-		.append("text")
-		.attr("class", "x-axis-label")
-		.attr("text-anchor", "middle")
-		.attr("x", chartToUpdate.width() / 2)
-		.attr("y", chartToUpdate.height())
-		.text(displayText);
-}
 
-function addYAxis(chartToUpdate, displayText) {
-	chartToUpdate
-		// .selectAll("g.row  text")
-		.append("text")
-		.attr("class", "y-axis-label")
-		.attr("text-anchor", "middle")
-		.attr("x", 0)
-		.attr("y", chartToUpdate.height()/2)
-		.text(displayText);
 }
